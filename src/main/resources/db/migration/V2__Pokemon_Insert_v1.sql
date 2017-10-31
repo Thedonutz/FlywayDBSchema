1c16e22bce3e4e99fc473b50f0e2ec383a041836
Jeff Morgan <Jeff.Morgan@Cerner.com>
2017-10-31 16:13:45 -0500

    added in example insert for pokemon tables.

---
+++ src/main/resources/db/migration/V2__Pokemon_Insert_v1.sql
@@ -0,0 +1,8 @@
+Insert into Pokemon ( PokeDex_Number, Name, Rarity, Sex )
+ values ( 1, 'Bulbasaur', 'R', 'M');
+
+Insert into Pokemon ( PokeDex_Number, Name, Rarity, Sex )
+ values ( 2, 'Ivysaur', 'R', 'M');
+ 
+Insert into Pokemon ( PokeDex_Number, Name, Rarity, Sex )
+ values ( 3, 'Venasaur', 'R', 'M');
