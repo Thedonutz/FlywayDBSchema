c9754c2cdaacd4f26b665aa04a208cbda2c1e69c
Jeff Morgan <Jeff.Morgan@Cerner.com>
2017-11-11 20:52:26 -0600

    types added.

---
+++ src/main/resources/db/migration/V8__Type_Insert1_v1.sql
@@ -0,0 +1,113 @@
+INSERT INTO Types (Name)
+VALUES ('Normal');
+
+INSERT INTO Types (Name)
+VALUES ('Normal/Flying');
+
+INSERT INTO Types (Name)
+VALUES ('Normal/Fairy');
+
+INSERT INTO Types (Name)
+VALUES ('Fight');
+
+INSERT INTO Types (Name)
+VALUES ('Fight/Water');
+
+INSERT INTO Types (Name)
+VALUES ('Flying/Poison');
+
+INSERT INTO Types (Name)
+VALUES ('Flying/Rock');
+
+INSERT INTO Types (Name)
+VALUES ('Flying/Bug');
+
+INSERT INTO Types (Name)
+VALUES ('Flying/Fire');
+
+INSERT INTO Types (Name)
+VALUES ('Flying/Water');
+
+INSERT INTO Types (Name)
+VALUES ('Flying/Electric');
+
+INSERT INTO Types (Name)
+VALUES ('Flying/Ice');
+
+INSERT INTO Types (Name)
+VALUES ('Flying/Dragon');
+
+INSERT INTO Types (Name)
+VALUES ('Poison');
+
+INSERT INTO Types (Name)
+VALUES ('Poison/Ground');
+
+INSERT INTO Types (Name)
+VALUES ('Poison/Bug');
+
+INSERT INTO Types (Name)
+VALUES ('Poison/Ghost');
+
+INSERT INTO Types (Name)
+VALUES ('Poison/Steel');
+
+INSERT INTO Types (Name)
+VALUES ('Poison/Water');
+
+INSERT INTO Types (Name)
+VALUES ('Poison/Grass');
+
+INSERT INTO Types (Name)
+VALUES ('Ground');
+
+INSERT INTO Types (Name)
+VALUES ('Ground/Rock');
+
+INSERT INTO Types (Name)
+VALUES ('Rock/Water');
+
+INSERT INTO Types (Name)
+VALUES ('Bug');
+
+INSERT INTO Types (Name)
+VALUES ('Bug/Grass');
+
+INSERT INTO Types (Name)
+VALUES ('Steel/Electric');
+
+INSERT INTO Types (Name)
+VALUES ('Fire');
+
+INSERT INTO Types (Name)
+VALUES ('Water');
+
+INSERT INTO Types (Name)
+VALUES ('Water/Psychic');
+
+INSERT INTO Types (Name)
+VALUES ('Water/Ice');
+
+INSERT INTO Types (Name)
+VALUES ('Grass');
+
+INSERT INTO Types (Name)
+VALUES ('Grass/Psychic');
+
+INSERT INTO Types (Name)
+VALUES ('Electric');
+
+INSERT INTO Types (Name)
+VALUES ('Psychic');
+
+INSERT INTO Types (Name)
+VALUES ('Psychic/Ice');
+
+INSERT INTO Types (Name)
+VALUES ('Psychic/Fairy');
+
+INSERT INTO Types (Name)
+VALUES ('Dragon');
+
+INSERT INTO Types (Name)
+VALUES ('Fairy');
